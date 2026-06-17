f1e78bac79698714f2ce08fb214eeea053c14bfb
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-06-17 11:33:53 +0500

    feat: show 404 when authenticated user tries to access /login

--- src/components/protect/GuestROute.tsx
+++ src/components/protect/GuestROute.tsx
@@ -1,23 +1,22 @@
 "use client";
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { notFound } from 'next/navigation';
 
 interface GuestRouteProps {
   children: React.ReactNode;
 }
 
 export default function GuestRoute({ children }: GuestRouteProps) {
-    const router = useRouter();
     const [isMounted, setIsMounted] = useState(false);
+    const [isAuth, setIsAuth] = useState(false);
 
     useEffect(() => {
         setIsMounted(true);
-        const auth = localStorage.getItem('accessToken');
-        if (auth) {
-            router.replace('/dashboard');
-        }
-    }, [router]);
+        const token = localStorage.getItem('accessToken');
+        setIsAuth(!!token);
+    }, []);
 
     if (!isMounted) return null;
+    if (isAuth) return notFound();
     return children;
-}
+}
