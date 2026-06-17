fe1372c677de081402b4f5eacbad6fa2ce42829b
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-06-17 10:43:10 +0500

    fix: bugs for building

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,40 @@
-// @ts-check
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import nextPlugin from '@next/eslint-plugin-next'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 
 export default defineConfig([
   globalIgnores(['dist', '.next']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': nextPlugin,
+      '@typescript-eslint': tsPlugin
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      globals: globals.browser,
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
     rules: {
       'react-refresh/only-export-components': 'off',
       'react-hooks/set-state-in-effect': 'off',
       'no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
+      ...tsPlugin.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
     }
   },
 ])
