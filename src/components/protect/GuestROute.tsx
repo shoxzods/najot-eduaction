8fd54b77c671e98de27cc2ac5ea3e3171f55751e
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-06-17 11:38:37 +0500

    fix: redirect authenticated users from /login to /dashboard

--- src/components/protect/GuestROute.tsx
+++ src/components/protect/GuestROute.tsx
@@ -1,22 +1,27 @@
 "use client";
 import { useEffect, useState } from 'react';
-import { notFound } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 interface GuestRouteProps {
   children: React.ReactNode;
 }
 
 export default function GuestRoute({ children }: GuestRouteProps) {
+    const router = useRouter();
     const [isMounted, setIsMounted] = useState(false);
     const [isAuth, setIsAuth] = useState(false);
 
     useEffect(() => {
         setIsMounted(true);
         const token = localStorage.getItem('accessToken');
-        setIsAuth(!!token);
-    }, []);
+        if (token) {
+            setIsAuth(true);
+            router.replace('/dashboard');
+        }
+    }, [router]);
 
     if (!isMounted) return null;
-    if (isAuth) return notFound();
+    if (isAuth) return null;
     return children;
-}
+}
+
